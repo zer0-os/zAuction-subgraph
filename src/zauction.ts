827298c45aff864160af04e2058dd16935e102ad
Zachary <remscar@gmail.com>
2021-08-30 17:26:04 -0700

    Initial Commit

---
+++ src/zauction.ts
@@ -0,0 +1,20 @@
+import { Account, TokenSale } from "../generated/schema";
+import { BidAccepted } from "../generated/ZAuction/ZAuction";
+import { uint256ToByteArray } from "./utils";
+
+export function handleBidAccepted(event: BidAccepted): void {
+  let bidder = new Account(event.params.bidder.toHex());
+  bidder.save();
+  let seller = new Account(event.params.seller.toHex());
+  seller.save();
+
+  let saleId = event.params.nftaddress + "-" + event.params.tokenid + "-" + event.block.timestamp;
+
+  let sale = new TokenSale(saleId);
+  sale.tokenId = uint256ToByteArray(event.params.tokenid).toHex();
+  sale.contractAddress = event.params.nftaddress;
+  sale.amount = event.params.amount;
+  sale.buyer = bidder.id;
+  sale.seller = seller.id;
+  sale.save();
+}
